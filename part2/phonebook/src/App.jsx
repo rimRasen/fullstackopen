7ad0793ed75e3032d61e8236cdcfa8937c1b284f
rimRasen <hany.sohi@gmail.com>
2025-03-09 20:28:36 +0100

    phonebook finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,13 @@
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
+import './index.css'
 import { useState, useEffect } from 'react'
 
 import axios from 'axios'
 import personService from './services/person'
+import person from './services/person'
 
 const App = () => {
   useEffect(() => { 
@@ -18,11 +21,16 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const  [notificationMessage, setNotificationMessage] = useState(null)
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.find(person => person.name === newName)) { 
-      alert(`${newName} is already added to phonebook`)
+    const existingPerson = persons.find(person => person.name === newName)
+    
+    if (existingPerson) { 
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) { 
+        updateNumber(existingPerson)
+      }
       return
     }
     const personObject = { 
@@ -36,6 +44,15 @@ const App = () => {
       setPersons(persons.concat(returnedPerson))
       setNewName('')
       setNewNumber('')
+
+      // Add notification when a new person is added
+      setNotificationMessage({
+        text: `Added ${returnedPerson.name}`,
+        type: 'success'
+      })
+      setTimeout(() => {
+        setNotificationMessage(null)
+      }, 5000)
     })
   }
 
@@ -47,6 +64,37 @@ const App = () => {
         setPersons(persons.filter(p => p.id !== person.id))
       })
     }
+  }
+
+  const updateNumber = (person) => {
+    const personObject = { 
+      ...person,
+      number: newNumber
+    }
+    personService
+    .update(person.id, personObject)
+    .then(returnedPerson => { 
+      setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+      setNewNumber('')
+
+      // Add notification when a person is updated
+      setNotificationMessage({ 
+        text: `Updated ${person.name}`,
+        type: 'success'
+      })
+    })
+    .catch(err => { 
+      setNotificationMessage({
+        text: `Cannot update ${person.name}, They may have been removed from the server`,
+        type: 'error'
+      })
+      setPersons(persons.filter(p => p.id !== person.id))
+    })
+    .finally(() => {
+      setTimeout(() => { 
+        setNotificationMessage(null)
+      }, 5000)
+    })
   }
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -61,6 +109,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notificationMessage}/>
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
