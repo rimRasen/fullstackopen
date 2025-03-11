cc87010e90da2caf294fb3b0fe928c3cf2d76d0c
rimRasen <hany.sohi@gmail.com>
2025-03-11 20:51:17 +0100

    fix

--- part2/data-for-countries/src/components/CountryDetail.jsx
+++ part2/data-for-countries/src/components/CountryDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
 const CountryDetail = ({ country }) => {
     const [weather, setWeather] = useState(null)
