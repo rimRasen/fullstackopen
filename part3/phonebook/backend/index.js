c2bf8544f4cf8936eccb78b5765e01f07e360edf
rimRasen <hany.sohi@gmail.com>
2025-03-23 21:20:26 +0100

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,51 +1,28 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import process from 'process';
 import mongoose from 'mongoose';
+import process from 'process';
+import 'dotenv/config'
 
-if (process.argv.length < 3) { 
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
 
-const db_password = process.argv[2]
-const url = `mongodb+srv://rimRasen:${db_password}@nodeexpressprojects.umpshdn.mongodb.net/fullstackopen?retryWrites=true&w=majority&appName=NodeExpressProjects`
+const url = process.env.MONGODB_URI
+console.log('Connecting to:', url)
 
 mongoose.connect(url)
+  .then(() => { 
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-name: String,
-number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
-
-if (process.argv.length === 3) {
-  // list all entries
-  Person.find({}).then(result => {
-  console.log('phonebook:')  
-  result.forEach(person => {
-      console.log(person)
-  })
-  mongoose.connection.close()
-  })
-} else if (process.argv.length === 5) {
-  // new entry
-  const name = process.argv[3]
-  const number = process.argv[4]
-  const person = new Person({
-      name: name,
-      number: number
-  })
-  person.save().then(() => {
-      console.log(`added ${name} number ${number} to phonebook`)
-      mongoose.connection.close()
-  })
-} else { 
-  console.log('Please provide the password, name and number as arguments: node mongo.js <password> <name> <number>')
-  process.exit(1)
-}
 
 const app = express();
 
@@ -63,27 +40,25 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-
-
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(result => {
-      res.json(result);
+    res.json(result);
   });
 });
 
 app.get('/api/persons/:id', (req, res) => { 
   Person.findById(req.params.id).then(person => {
-      if (person) {
-          res.json(person);
-      } else {
-          res.status(404).end();
-      }
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
   });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
   Person.findByIdAndDelete(req.params.id).then(() => {
-      res.status(204).end();
+    res.status(204).end();
   });
 });
 
@@ -91,28 +66,28 @@ app.post('/api/persons', (req, res) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
-      return res.status(400).json({
-          error: 'The name or number missing'
-      });
+    return res.status(400).json({
+      error: 'The name or number missing'
+    });
   }
 
   const person = new Person({
-      name: body.name,
-      number: body.number
+    name: body.name,
+    number: body.number
   });
 
   person.save().then(savedPerson => {
-      res.json(savedPerson);
+    res.json(savedPerson);
   });
 });
 
 app.get('/info', (req, res) => {
   Person.countDocuments({}).then(count => {
-      const info = `
+    const info = `
       <p>Phonebook has info for ${count} people</p>
       <p>${new Date()}</p>
-      `;
-      res.send(info);
+    `;
+    res.send(info);
   });
 });
 
