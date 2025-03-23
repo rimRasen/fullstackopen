05412e6fe126436df428a01a96788c4f03948395
rimRasen <hany.sohi@gmail.com>
2025-03-23 20:49:43 +0100

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,6 +2,50 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import process from 'process';
+import mongoose from 'mongoose';
+
+if (process.argv.length < 3) { 
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const db_password = process.argv[2]
+const url = `mongodb+srv://rimRasen:${db_password}@nodeexpressprojects.umpshdn.mongodb.net/fullstackopen?retryWrites=true&w=majority&appName=NodeExpressProjects`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+name: String,
+number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  // list all entries
+  Person.find({}).then(result => {
+  console.log('phonebook:')  
+  result.forEach(person => {
+      console.log(person)
+  })
+  mongoose.connection.close()
+  })
+} else if (process.argv.length === 5) {
+  // new entry
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const person = new Person({
+      name: name,
+      number: number
+  })
+  person.save().then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+  })
+} else { 
+  console.log('Please provide the password, name and number as arguments: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
 
 const app = express();
 
@@ -19,90 +63,60 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
-})
+  Person.find({}).then(result => {
+      res.json(result);
+  });
+});
 
 app.get('/api/persons/:id', (req, res) => { 
-  const id = req.params.id;
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
-})
+  Person.findById(req.params.id).then(person => {
+      if (person) {
+          res.json(person);
+      } else {
+          res.status(404).end();
+      }
+  });
+});
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const personToDelete = persons.find(person => person.id === id);
-
-  if (personToDelete) {
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end();
-  } else { 
-    res.status(404).end();
-  }
-})
+  Person.findByIdAndDelete(req.params.id).then(() => {
+      res.status(204).end();
+  });
+});
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: 'The name or number missing'
-    });
-  }
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({
-      error: 'The name already exists in the phonebook'
-    });
+      return res.status(400).json({
+          error: 'The name or number missing'
+      });
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: String(Math.floor(Math.random() * 5000))
-  };
+  const person = new Person({
+      name: body.name,
+      number: body.number
+  });
 
-  persons = persons.concat(person);
-  res.json(person);
-})
+  person.save().then(savedPerson => {
+      res.json(savedPerson);
+  });
+});
 
 app.get('/info', (req, res) => {
-  const info = `
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  `;
-
-  res.send(info);
-})
+  Person.countDocuments({}).then(count => {
+      const info = `
+      <p>Phonebook has info for ${count} people</p>
+      <p>${new Date()}</p>
+      `;
+      res.send(info);
+  });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => { 
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
